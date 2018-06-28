4257f780895d9622ca0455f44d461268d1d90519
cybermerlin <cybermerlin@users.noreply.github.com>
2018-06-28 23:44:09 +0300

    Update stand.js
    fix define additional functional into String, Array

--- plugins/core/stand.js
+++ plugins/core/stand.js
@@ -1,28 +1,32 @@
 if ( !String.prototype.format )
-  String.prototype.format = function(values) {
-    var a = arguments;
-    return this.replace(/\{(\d+)\}/g, function(m, i) {
-      return a[i];
-    });
-  };
-if ( !Array.prototype.indexOfKey )
-  Array.prototype.indexOfKey = function(key, val, from) {
-    var len = this.length;
-    from = Number(arguments[2]) || 0;
-    from = (from < 0)
-        ? Math.ceil(from)
-        : Math.floor(from);
-    if ( from < 0 ) from += len;
-
-    for (; from < len; from++) {
-      try {
-        if ( key in this[from] ) {
-          if ( this[from][key] === val ) return from;
-          if ( typeof (this[from][key]) === 'number' || typeof (val) === 'number' )
-            if ( parseInt(val) === parseInt(this[from][key]) )
-              return from;
-        }
-      } catch (e) {}
+  Object.defineProperty(String.prototype, 'format', {
+    value: function(values) {
+      var a = arguments;
+      return this.replace(/\{(\d+)\}/g, function(m, i) {
+        return a[i];
+      });
     }
-    return -1;
-  };
+  });
+if ( !Array.prototype.indexOfKey )
+  Object.defineProperty(Array.prototype, 'indexOfKey', {
+    value: function(key, val, from) {
+      var len = this.length;
+      from = Number(arguments[2]) || 0;
+      from = (from < 0)
+          ? Math.ceil(from)
+          : Math.floor(from);
+      if ( from < 0 ) from += len;
+
+      for (; from < len; from++) {
+        try {
+          if ( key in this[from] ) {
+            if ( this[from][key] === val ) return from;
+            if ( typeof (this[from][key]) === 'number' || typeof (val) === 'number' )
+              if ( parseInt(val) === parseInt(this[from][key]) )
+                return from;
+          }
+        } catch (e) {}
+      }
+      return -1;
+    }
+  });
